c82835631b7a7132b84bab9d2c56c5431c3e8f1f
Brandon Sprague <brandon@sprague.mx>
2024-11-24 16:55:22 -0800

    Tweaks to worker for debugging

--- worker/worker.py
+++ worker/worker.py
@@ -90,7 +90,7 @@ def receive_frames_from_renderer(renderer_name, json_payload=None):
         return
 
 
-def send_frame_to_display(pillow_raw_image_data):
+def send_frame_to_display(pillow_raw_image_data, i):
     # FIXME
     # FIXME
     # FIXME
@@ -104,30 +104,30 @@ def send_frame_to_display(pillow_raw_image_data):
     # assert that its mode is 1 i.e. black and white
     assert pil_image.mode == "1"
 
-    frame = np.zeros((SCREEN_HEIGHT * SCREEN_WIDTH, 1), bool)
-
     np_image = np.asarray(pil_image)
 
     # flip img from right-left to left-right
     np_image = np.flip(np_image, 1)
 
-    frame_packed_bits = np.packbits(
-        np_image.astype("bool"), axis=None, bitorder="little"
-    )
-
     if DO_NOT_SEND_TO_RITER:
         # go through the lines and columns
         # and print '.' for 0 bit and '#' for 1 bit
         # clear terminal!!!!!
-        print("\033c")
+        print("\033c", end="")
+        parts = []
         for y in range(SCREEN_HEIGHT):
             for x in range(SCREEN_WIDTH - 1, 0, -1):
                 if np_image[y][x] == 1:
-                    print("#", end="")
+                    parts.append('#')
                 else:
-                    print(".", end="")
-            print()
+                    parts.append('.')
+            parts.append('\n')
+        log.info(''.join(parts))
+        pass
     else:
+        frame_packed_bits = np.packbits(
+            np_image.astype("bool"), axis=None, bitorder="little"
+        )
         SCREEN_SOCK.sendto(frame_packed_bits, (SCREEN_UDP_IP, SCREEN_UDP_PORT))
 
 
@@ -173,12 +173,14 @@ def worker():
             print("show type", show["show_type"])
             print("show", json.dumps(show)[:100], "...")
 
+            i = 0
             for frame in receive_frames_from_renderer(
                 show["show_type"], json_payload=show["payload"]
             ):
                 # print("receive_frames_from_renderer")
                 # we processed 1 frame, we can do other things now
-                send_frame_to_display(frame)
+                i += 1
+                send_frame_to_display(frame, i)
 
                 # get any event from the queue, but don't block if there's no event
                 event = None
